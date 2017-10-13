6318ef4dd9d1cf7a7ac96ada67e194934e14f8a9
Alek Hristov <alek.hristov@naxex-tech.com>
2017-10-13 15:32:43 +0300

    Entity Framework in action

--- Interfaces/OperationContracts/IChatService.cs
+++ Interfaces/OperationContracts/IChatService.cs
@@ -8,5 +8,27 @@ namespace Interfaces.OperationContracts
 {
     public interface IChatService
     {
+        IConnectResponse Connect(IConnectRequest request);
+
+        IChatHistoryResponse GetChatHistory(IChatHistoryRequest request)
+        {
+            var list<Messages> = ...
+            var list<IMessages> mapper.Convert(list<Messages>);
+        }
+    }
+
+    class ChatHistoryResponse
+    {
+        IList<IMessages>
+    }
+
+    interface IMessages
+    {
+        public string Message { get; set; }
+    }
+
+    public class Message : IMessages
+    {
+        public string Message { get; set; }
     }
 }
