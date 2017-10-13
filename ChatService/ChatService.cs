f435369e7a15c6ca5e15345e0b95f187d474be0a
Alek Hristov <alek.hristov@naxex-tech.com>
2017-10-13 15:20:44 +0300

    First project commit

---
+++ ChatService/ChatService.cs
@@ -0,0 +1,12 @@
+﻿using Interfaces.OperationContracts;
+
+namespace ChatService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ChatService" in both code and config file together.
+    public class ChatService : IChatService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
